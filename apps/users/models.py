0c32792db8b206d2118f87afce56cb5196a9ad94
jihuixin <jihuixin@xjgreat.com>
2018-03-15 18:05:06 +0800

    修改邮箱验证码model的verbose_name

--- apps/users/models.py
+++ apps/users/models.py
@@ -17,7 +17,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default='')
     birday = models.DateField(verbose_name=u'生日', null=True, blank=True)
-    gender = models.CharField(max_length=5, choices=(('male', u'男'), ('female', u'女')), default='female')
+    gender = models.CharField(max_length=10, choices=(('male', u'男'), ('female', u'女')), default='female')
     address = models.CharField(max_length=100, default=u'')
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y%m", default=u'image/default.png', max_length=100)
@@ -33,8 +33,8 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name=u'验证码')
     email = models.EmailField(max_length=50, verbose_name=u'邮箱')
-    send_type = models.CharField(max_length=10, choices=(('register', u'注册'), ('forget', u'找回密码')))
-    send_time = models.DateTimeField(default=datetime.now)  # 注:datetime.now不带括号,如果带括号显示的时间就是这个model被编译的时间
+    send_type = models.CharField(verbose_name=u'验证码类型', max_length=10, choices=(('register', u'注册'), ('forget', u'找回密码')))
+    send_time = models.DateTimeField(verbose_name=u'发送时间', default=datetime.now)  # 注:datetime.now不带括号,如果带括号显示的时间就是这个model被编译的时间
 
     class Meta:
         verbose_name = u'邮箱验证码'
