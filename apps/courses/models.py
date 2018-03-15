8c74e622aab01ea88e78f3771ff4752fa0c2589e
jihuixin <jihuixin@xjgreat.com>
2018-03-15 22:39:10 +0800

    把课程模块下的model和admin进行关联注册

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -14,7 +14,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'课程名称')
     desc = models.CharField(max_length=300, verbose_name=u'课程描述')
     detail = models.TextField(verbose_name=u'课程详情')  # 不用char而使用text类型不需要限制最大长度
-    degree = models.CharField(choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')),max_length=10)
+    degree = models.CharField(verbose_name=u'难度', choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')), max_length=10)
     learn_times = models.IntegerField(default=0, verbose_name=u'学习时长')
     students = models.IntegerField(default=0, verbose_name=u'学习人数')
     fav_nums = models.IntegerField(default=0, verbose_name=u'收藏人数')
@@ -26,14 +26,21 @@ class Course(models.Model):
         verbose_name = u'课程'
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u'课程')
     name = models.CharField(max_length=100, verbose_name=u'章节名')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
     class Meta:
         verbose_name = u'章节'
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '{}({})'.format(self.course, self.name)
 
 
 class Video(models.Model):
@@ -47,7 +54,7 @@ class Video(models.Model):
 
 
 class CourseResource(models.Model):
-    course = models.ForeignKey(Course, verbose_name=u'章节')
+    course = models.ForeignKey(Course, verbose_name=u'课程')
     name = models.CharField(max_length=100, verbose_name=u'资源名称')
     download = models.FileField(upload_to='course/resource/%Y/%m', verbose_name='资源文件', max_length=100)
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
