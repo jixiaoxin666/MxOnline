0255bb0ca0c8538133814952f100f86c54bb5354
jihuixin <jihuixin@xjgreat.com>
2018-04-01 13:50:27 +0800

    xadmin端 设置app的显示名字为中文

---
+++ apps/operation/__init__.py
@@ -0,0 +1,2 @@
+# 设置该app的配置来源
+default_app_config = "operation.apps.OperationConfig"
--- apps/operation/apps.py
+++ apps/operation/apps.py
@@ -1,5 +1,7 @@
+# coding:utf-8
 from django.apps import AppConfig
 
 
 class OperationConfig(AppConfig):
     name = 'operation'
+    verbose_name = u'用户操作'  # 把app在xadmin中显示为中文
